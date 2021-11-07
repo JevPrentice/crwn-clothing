6fbfcc3a2e8a6938b097c9f28af4a3b2e285a5ca
Jev Prentice <jevprentice@gmail.com>
2021-11-07 12:58:04 +0000

    shop page, collection preview, collection item

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import {Route, Routes} from 'react-router-dom';
-
-const HatPage = () => <div><h1>Hats!</h1></div>
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
     return <div>
         <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route exact path='/hats' element={<HatPage/>}/>
+            <Route exact path="/" element={<HomePage/>}/>
+            <Route path='/shop' element={<ShopPage/>}/>
         </Routes>
     </div>;
 }
