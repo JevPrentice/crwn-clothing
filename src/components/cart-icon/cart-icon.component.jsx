cef867ffef54498d84cd33a1524f6680153b17ba
Jev Prentice <jevprentice@gmail.com>
2021-11-09 12:27:54 +0000

    added reselect, updated cart dropdown and cart icon with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,6 +5,7 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import "./cart-icon.styles.scss"
 import {connect} from "react-redux";
 import {toggleCartHidden} from "../../redux/cart/cart.actions";
+import {selectCartItemsCount} from "../../redux/cart/cart.selectors";
 
 const CartIcon = ({toggleCartHidden, itemCount}) => {
     return <div className='cart-icon' onClick={toggleCartHidden}>
@@ -13,9 +14,8 @@ const CartIcon = ({toggleCartHidden, itemCount}) => {
     </div>
 };
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    itemCount: cartItems.reduce(
-        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
 });
 
 const mapDispatchToProps = dispatch => ({
