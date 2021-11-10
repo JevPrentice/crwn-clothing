51a3bb1ff20a3daabeff78558faa7605a7af2b50
Jev Prentice <jevprentice@gmail.com>
2021-11-10 08:03:21 +0000

    fix header styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -17,10 +17,10 @@ const Header = ({currentUser, hidden}) =>
             <Logo className='logo'/>
         </Link>
         <div className='options'>
-            <Link className='options' to='/shop'>
+            <Link className='option' to='/shop'>
                 SHOP
             </Link>
-            <Link className='options' to='/content'>
+            <Link className='option' to='/content'>
                 CONTENT
             </Link>
             {
