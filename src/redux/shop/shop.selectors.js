2166a0e4abb53038e21cd53a073aa58f58ced9c0
Jev Prentice <jevprentice@gmail.com>
2021-11-10 07:39:01 +0000

    memoizing shop collections

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,5 @@
 import {createSelector} from "reselect";
+import memoize from 'lodash.memoize';
 
 const COLLECTION_ID_MAP = {
     hats: 1,
@@ -13,6 +14,7 @@ const selectShop = state => state.shop;
 export const selectCollections = createSelector([selectShop],
     shop => shop.collections);
 
-export const selectCollection = collectionUrlParam => createSelector([selectCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+export const selectCollection = memoize(collectionUrlParam => createSelector([selectCollections],
+    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]))
+    // lodash.memoize: here we are memoizing the return of our function
 );
