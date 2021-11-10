d34d3185f8a5147bf6e7a0007e2fc8e6975f68d1
Jev Prentice <jevprentice@gmail.com>
2021-11-10 15:12:08 +0000

    remove code to upload json data to firebase

--- src/App.js
+++ src/App.js
@@ -7,21 +7,20 @@ import {setCurrentUser} from "./redux/user/user.actions";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import {addCollectionAndDocuments, auth, createUserProfileFirebaseDocument} from "./firebase/firebase.utils"
+import {auth, createUserProfileFirebaseDocument} from "./firebase/firebase.utils"
 
 import './App.css';
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {createStructuredSelector} from "reselect";
 import {selectCurrentUser} from "./redux/user/user.selector";
 import CheckoutPage from "./pages/checkout/checkout.component";
-import {selectCollectionsForPreview} from "./redux/shop/shop.selectors";
 
 class App extends React.Component {
 
     unsubscribeFromAuth = null
 
     componentDidMount() {
-        const {setCurrentUser, collectionsArray} = this.props
+        const {setCurrentUser} = this.props
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
                 const userRef = await createUserProfileFirebaseDocument(userAuth, {});
@@ -33,10 +32,6 @@ class App extends React.Component {
                 });
             }
             setCurrentUser(userAuth)
-            addCollectionAndDocuments("collections", collectionsArray.map(({title, items}) => ({
-                title: title,
-                items: items
-            }))).then(r => console.log('done!'));
         });
     }
 
@@ -58,8 +53,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = ({user}) => createStructuredSelector({
-    currentUser: selectCurrentUser,
-    collectionsArray: selectCollectionsForPreview
+    currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
