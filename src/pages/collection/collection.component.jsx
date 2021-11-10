2e59fa1a3807b76feaf4d878b91b542d2d5fcfce
Jev Prentice <jevprentice@gmail.com>
2021-11-10 08:00:47 +0000

    data normalization and collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,24 +3,22 @@ import './collection.styles.scss'
 import {useParams} from "react-router-dom";
 import {selectCollection} from "../../redux/shop/shop.selectors";
 import {connect} from "react-redux";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
 const CollectionPage = state => {
     const params = useParams();
     const {collectionId} = params;
     const collection = selectCollection(collectionId)(state);
-    console.log(collection)
-    return <div className='collection'>
-        <h2>{collectionId}</h2>
+    const {title, items} = collection;
+    return <div className='collection-page'>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>{
+            items.map(item => <CollectionItem key={item.id} item={item}/>)
+        }</div>
     </div>;
 };
 
 const mapStateToProps = state => state;
-
-// const mapStateToProps = (state) => {
-//     return ({
-//         state: state
-//     });
-// };
 
 export default connect(mapStateToProps)(CollectionPage);
 
