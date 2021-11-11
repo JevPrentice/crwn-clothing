103a22f1a4d7a7ef7f79a6a1c7ccd6e2b869bc22
Jev Prentice <jevprentice@gmail.com>
2021-11-11 14:49:33 +0000

    initial saga for shop data

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,17 +2,23 @@ import {applyMiddleware, createStore} from 'redux';
 import {persistStore} from 'redux-persist';
 import {logger} from 'redux-logger';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./root-reducer"
+import rootSaga from "./root-saga";
 
 const middlewares = [];
+const sagaMiddleware = createSagaMiddleware();
 
 if (process.env.NODE_ENV === 'development')
     middlewares.push(logger);
 
 middlewares.push(thunk);
+middlewares.push(sagaMiddleware);
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
