ac1130694b718056fe5046d86dea04f01c7b9b3c
Jev Prentice <jevprentice@gmail.com>
2021-11-11 08:15:02 +0000

    read shop data from firebase, add with spinner component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,25 +5,49 @@ import CollectionPage from "../collection/collection.component";
 import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
 import {connect} from "react-redux";
 import {UpdateCollections} from "../../redux/shop/shop.actions";
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+
+    state = {
+        loading: true
+    }
 
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
         const {updateCollections} = this.props;
         const collectionRef = firestore.collection('collections');
+        // Observable pattern -> live streaming updates.
         this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
+            this.setState({loading: false});
         });
+        // Promise style.
+        // collectionRef.get().then(snapshot => {
+        //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        //     updateCollections(collectionsMap);
+        //     this.setState({loading: false});
+        // });
+        // Native 'fetch' style.
+        // fetch("https://firestore.googleapis.com/v1/projects/crwn-db-325ea/databases/(default)/documents/collections")
+        //     .then(response => response.json())
+        //     .then(response => {
+        //         console.log(response);
+        //     })
+        //     .catch(e => console.log(e));
     }
 
     render() {
+        const {loading} = this.state;
         return <div className='shop-page'>
             <Routes>
-                <Route path="/" element={<CollectionsOverview/>}/>
-                <Route path=':collectionId' element={<CollectionPage/>}/>
+                <Route path="/" element={<CollectionsOverviewWithSpinner isLoading={loading}/>}/>
+                <Route path=':collectionId' element={<CollectionPageWithSpinner isLoading={loading}/>}/>
             </Routes>
         </div>;
     }
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -8,12 +8,15 @@ export const selectCollections = createSelector([selectShop],
 
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
-    collections => Object.keys(collections).map(key => collections[key])
+    collections => collections
+        ? Object.keys(collections).map(key => collections[key])
+        : []
 );
 
 export const selectCollection = memoize(collectionUrlParam =>
         createSelector([selectCollections],
-            collections =>
-                collections[collectionUrlParam])
+            collections => collections
+                ? collections[collectionUrlParam]
+                : null)
     // lodash.memoize: here we are memoizing the return of our function
 );
