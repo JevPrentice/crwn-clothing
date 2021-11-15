999222fca4e8c32513f85fda6ef057cfdff660c9
Jev Prentice <jevprentice@gmail.com>
2021-11-15 10:16:12 +0000

    useDispatch instead of connect on app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, {useEffect} from 'react';
 import {Navigate, Route, Routes} from 'react-router-dom';
-
-import {connect} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {selectCurrentUser} from "./redux/user/user.selector";
+import {checkUserSession} from "./redux/user/user.actions";
 
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 
 import './App.css';
-import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {createStructuredSelector} from "reselect";
-import {selectCurrentUser} from "./redux/user/user.selector";
-import CheckoutPage from "./pages/checkout/checkout.component";
-import {checkUserSession} from "./redux/user/user.actions";
 
-const App = ({checkUserSession, currentUser}) => {
+const App = () => {
+    const currentUser = useSelector(selectCurrentUser);
+    // const isHidden = useSelector((state) => state.cart.hidden);
+    const dispatch = useDispatch();
 
-    useEffect(() => checkUserSession(), [checkUserSession]);
+    useEffect(() => {
+        dispatch(checkUserSession());
+    }, [dispatch]);
 
     return <div>
         <Header/>
@@ -29,12 +32,4 @@ const App = ({checkUserSession, currentUser}) => {
     </div>;
 }
 
-const mapStateToProps = createStructuredSelector({
-    currentUser: selectCurrentUser
-});
-
-const mapDispatchToProps = dispatch => ({
-    "checkUserSession": () => dispatch(checkUserSession())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
