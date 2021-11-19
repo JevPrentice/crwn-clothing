7885a018437312257fcf4695094a55dccb87fcf4
Jev Prentice <jevprentice@gmail.com>
2021-11-19 15:08:29 +0000

    update server.js

--- server.js
+++ server.js
@@ -15,13 +15,11 @@ const compression = require('compression');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(enforce.HTTPS());
-app.use(enforce.HTTPS({trustProtoHeader: true}));
-
-app.use(compression());
-
 if (process.env.NODE_ENV === 'production') {
+    app.use(compression());
+    app.use(enforce.HTTPS({trustProtoHeader: true}));
     app.use(express.static(path.join(__dirname, 'client/build')));
+    
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build/index.html'));
     });
